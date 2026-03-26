f828475f351e100ec7b73d138b18ebca79ff5848
Major Hayden <major@redhat.com>
2026-03-26 12:27:34 -0500

    feat(rag): refactor deduplicate_chunks for radon compliance and export new public API
    Signed-off-by: Major Hayden <major@redhat.com>

--- src/okp_mcp/rag/__init__.py
+++ src/okp_mcp/rag/__init__.py
@@ -1,5 +1,7 @@
 """Query functions for the portal-rag Solr core."""
 
+from .common import RAG_FL, clean_rag_query
+from .formatting import deduplicate_chunks, format_rag_result
 from .hybrid import hybrid_search
 from .lexical import lexical_search
 from .models import RagDocument, RagResponse
@@ -7,8 +9,12 @@ from .rrf import reciprocal_rank_fusion
 from .semantic import semantic_search, semantic_text_search
 
 __all__ = [
+    "RAG_FL",
     "RagDocument",
     "RagResponse",
+    "clean_rag_query",
+    "deduplicate_chunks",
+    "format_rag_result",
     "hybrid_search",
     "lexical_search",
     "reciprocal_rank_fusion",
